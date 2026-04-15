176dd3f37112cc73fd254f935a6b2af90029a29b
mikpavel <mikpavel@cvut.cz>
2026-04-15 15:27:50 +0200

    Fix gitignore: remove build and dependencies

--- dist/background.js
+++
@@ -1,1 +0,0 @@
-var e=[];chrome.webRequest.onCompleted.addListener(t=>{e.push(t.url)},{urls:[`<all_urls>`]}),chrome.runtime.onMessage.addListener((t,n,r)=>{t.type===`GET_REQUESTS`&&r({requests:e})});
